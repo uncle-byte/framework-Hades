77a339c8475707dd5b1e8f7202817e374539a903
chenhaipeng <369161356@qq.com>
2016-11-10 20:41:59 +0800

    init commit

---
+++ commons/src/test/java/com/framework/utils/StringHelperTest.java
@@ -0,0 +1,242 @@
+package com.framework.utils;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * StringHelper Tester.
+ *
+ * @author chenhaipeng
+ * @version 1.0
+ * @since <pre>十一月 10, 2016</pre>
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath*:applicationContext.xml"})
+public class StringHelperTest {
+
+	@Before
+	public void before() throws Exception {
+	}
+
+	@After
+	public void after() throws Exception {
+	}
+
+
+	/**
+	 * Method: removeCrlf(String str)
+	 */
+	@Test
+	public void testRemoveCrlf() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: unescapeXml(String str)
+	 */
+	@Test
+	public void testUnescapeXml() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: removePrefix(String str, String prefix)
+	 */
+	@Test
+	public void testRemovePrefix() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: isBlank(String str)
+	 */
+	@Test
+	public void testIsBlank() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: isNotBlank(String str)
+	 */
+	@Test
+	public void testIsNotBlank() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: getExtension(String str)
+	 */
+	@Test
+	public void testGetExtension() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: countOccurrencesOf(String str, String sub)
+	 */
+	@Test
+	public void testCountOccurrencesOf() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: contains(String str, String... keywords)
+	 */
+	@Test
+	public void testContains() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: defaultString(Object value)
+	 */
+	@Test
+	public void testDefaultString() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: defaultIfEmpty(Object value, String defaultValue)
+	 */
+	@Test
+	public void testDefaultIfEmpty() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: makeAllWordFirstLetterUpperCase(String sqlName)
+	 */
+	@Test
+	public void testMakeAllWordFirstLetterUpperCase() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: indexOfByRegex(String input, String regex)
+	 */
+	@Test
+	public void testIndexOfByRegex() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: toJavaVariableName(String str)
+	 */
+	@Test
+	public void testToJavaVariableName() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: toJavaClassName(String str)
+	 */
+	@Test
+	public void testToJavaClassName() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: removeMany(String inString, String... keywords)
+	 */
+	@Test
+	public void testRemoveMany() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: replace(String inString, String oldPattern, String newPattern)
+	 */
+	@Test
+	public void testReplace() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: capitalize(String str)
+	 */
+	@Test
+	public void testCapitalize() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: uncapitalize(String str)
+	 */
+	@Test
+	public void testUncapitalize() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: randomNumeric(int count)
+	 */
+	@Test
+	public void testRandomNumeric() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: random(int count, boolean letters, boolean numbers)
+	 */
+	@Test
+	public void testRandom() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: toUnderscoreName(String name)
+	 */
+	@Test
+	public void testToUnderscoreName() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: removeEndWiths(String inputString, String... endWiths)
+	 */
+	@Test
+	public void testRemoveEndWiths() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: substringMatch(CharSequence str, int index, CharSequence substring)
+	 */
+	@Test
+	public void testSubstringMatch() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: tokenizeToStringArray(String str, String seperators)
+	 */
+	@Test
+	public void testTokenizeToStringArray() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: join(List list, String seperator)
+	 */
+	@Test
+	public void testJoin() throws Exception {
+		//TODO: Test goes here...
+	}
+
+	/**
+	 * Method: containsCount(String string, String keyword)
+	 */
+	@Test
+	public void testContainsCount() throws Exception {
+		//TODO: Test goes here...
+	}
+
+
+	/**
+	 * Method: changeFirstCharacterCase(String str, boolean capitalize)
+	 */
+	@Test
+	public void testChangeFirstCharacterCase() throws Exception {
+	}
+} 
